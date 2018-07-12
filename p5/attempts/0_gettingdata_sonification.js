ca745c891af329fe2c57a0dd7544178a4ee3e157
dreamon1321 <andrea.tunhi@gmail.com>
2018-07-12 14:02:28 -0400

    Direct sonification available

--- p5/attempts/0_gettingdata_sonification.js
+++ p5/attempts/0_gettingdata_sonification.js
@@ -1,98 +1,121 @@
 // Table Data variable
+var url;
 var data;
+var rowNum;
+var colNum;
 
 // For automatically playing the song
-// var index = 0;
-// var trigger = 0;
-// var autoplay = true;
+var rowIndex = 0;
+var trigger = 0;
+var autoplay = true;
 
 // Set a total length of 1 minute = 60 s = 60000 millis
-// var total_time = 50000;
-// var tick;
-// var osc;
+var total_time = 50000;
+var tick;
+var osc;
 
 // Mins / Maxs
-// var price_min;
-// var price_max;
-// var delinq_min;
-// var delinq_max;
-// var sold_min;
-// var sold_max;
+var price_min;
+var price_max;
+var delinq_min;
+var delinq_max;
+var sold_min;
+var sold_max;
 
 function preload() {
-	var url = 'https://github.com/hopelessoptimism/data-sonification/blob/master/Housing/data/housing_bubble.csv';
+	url = 'https://raw.githubusercontent.com/hopelessoptimism/data-sonification/master/data/housing_bubble.csv';
 	data = loadTable(url, 'csv', 'header');
 }
 
 // SETUP only occurs once
 function setup() {
-	var rowNum = data.getRowCount();
-	var colNum = data.getColumnCount();
+	rowNum = data.getRowCount();
+	colNum = data.getColumnCount();
 
 	console.log(rowNum + ' rows\n' + colNum + ' columns');
 
+	// Min and max house_price_index		***MATH.MIN/MAX WILL RETURN NAN
+	price_min = min(data.getColumn('house_price_index'));
+	price_max = max(data.getColumn('house_price_index'));
+	
+	// Min and max delinquency
+	delinq_min = min(data.getColumn('delinquency'));
+	delinq_max = max(data.getColumn('delinquency'));
+
+	// Min and max houses_sold
+	sold_min = min(data.getColumn('houses_sold'));
+	sold_max = max(data.getColumn('houses_sold'));
+
 	// TODO: compute the note duration (total_time / rowNum)
-	// TODO: find the min and max house_price_index
-	// TODO: find the min and max delinquency
-	// TODO: find the min and max hosues_sold
+	tick = total_time / data.getRowCount();
 
 	// debugging statements
-	// console.log('Total Duration: ' + tick);
-	// console.log('Price Min: ' + price_min);
-	// console.log('Price Max: ' + price_max);
-	// console.log('Delinquent Min: ' + delinq_min);
-	// console.log('Delinquent Max: ' + delinq_max);
-	// console.log('Sold Min: ' + sold_min);
-	// console.log('Sold Max: ' + sold_max);
+	console.log('Total Duration: ' + tick);
 
-	// A Sine Oscillator
-	// osc1 = new p5.SinOsc();
+	// A Sine Oscillator, for House Price Index
+	osc1 = new p5.TriOsc();
 
-	// TODO: create a second oscillator
-	// osc2 = new p5.SinOsc();
+	// Second oscillator, for Delinquency
+	osc2 = new p5.SinOsc();
 
 	// Start the oscillators silent
-	// osc1.start();
-	// osc2.start();
-	// osc1.amp(0);
-	// osc2.amp(0);
+	osc1.start();
+	osc2.start();
+	osc1.amp(0);
+	osc2.amp(0);
 }
 
 // DRAW occurs continuously, order is important
 function draw() {
 	// If autoplaying and it's time for the next note
-	// if (autoplay && (millis() > trigger)) {
-	// 	TODO: play a note with the `house_price_index` column value
-	// 	TODO: play a second voicing according to the `delinquency` rate
-	// 	TODO: modulate the master volume according to the `houses_sold` column
+	if (autoplay && (millis() > trigger)) {
 
-	// 	advance trigger by 'duration'
-	// 	trigger = millis() + tick;
+	// 	Play a note with the 'house_price_index' column value
+		playNote(data.get(rowIndex, 'house_price_index'), 500, osc1, price_min, price_max);
+
+	// 	Play a second voicing according to the `delinquency` rate
+		playNote(data.get(rowIndex, 'delinquency'), 500, osc2, delinq_min, delinq_max);
+
+	// 	Modulate the master volume according to the `houses_sold` column
+	var volume = map(data.get(rowIndex, 'houses_sold'), sold_min, sold_max, 0.25, 1.0);
+	masterVolume(volume);
+	//console.log(volume);
+
+	// 	Advance trigger by 'duration'
+	trigger = millis() + tick;
 
 	// 	TODO: drop the bass when the market crashes
-	// 	TODO: advance the index by one to the next note / data point
-	// } else if (index >= data.getRowCount()) {
-	// 	we're at the end of the data file, stop autoplaying
-	// 	autoplay = false;
-	// 	oscillators.stop();
-	// }
+
+	//  Advance the index by one to the next note / data point
+	rowIndex++;
+
+	} else if (rowIndex >= data.getRowCount()) {
+	// 	At the end of the data file, stop autoplaying
+	autoplay = false;
+	osc1.stop();
+	osc2.stop();
+	}
+
 }
 
 function playNote(position, duration, osc, lower, upper) {
-	// TODO: map the data point to a corresponding MIDI note
+	// Map the data point to a corresponding MIDI note
 	// http://newt.phys.unsw.edu.au/jw/notes.html
-	midi = 65;
+	// map(value, start1, stop1, midiMin, midiMax, withinBounds);
+	// MIDI 43 = ~100 Hz		MIDI 96 = ~2,000 Hz
+	midi = round(map(position, lower, upper, 43, 96, true))
 
 	// TODO: set the oscilator frequency to play the midi note
+	osc.freq(midiToFreq(midi));
 
 	// Fade in the amplitude 
-	// osc.fade(0.5, 0.2);
+	osc.fade(0.5, 0.2);
 
 	// Fade out after duration
-	// if (duration) {
-	// 	setTimeout(function() {
-	// 		osc.fade(0, 0.2);
-	// 	}, duration - 50);
-	// }
+	if (duration) {
+		setTimeout(function() {
+			osc.fade(0, 0.2);
+		}, duration - 50);
+	}
 
 }
